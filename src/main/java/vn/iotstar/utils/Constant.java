0ba1167c49174eb93f64df68fc8d2bd246736e35
PhucThanh2004 <lathanhbatri2020@gmail.com>
2024-10-09 13:26:32 +0700

    Category, Video

---
+++ src/main/java/vn/iotstar/utils/Constant.java
@@ -0,0 +1,12 @@
+package vn.iotstar.utils;
+
+public class Constant {
+	public static final String SESSION_USERNAME = "username";
+	public static final String COOKIE_REMEMBER = "username";
+	//public static final String REGISTER = "/views/register.jsp";
+	
+	 public static class Path {
+	        public static final String REGISTER = "/views/register.jsp"; // Đường dẫn đến trang đăng ký
+	    }
+	 public static String DIR = "C:\\upload";
+}
